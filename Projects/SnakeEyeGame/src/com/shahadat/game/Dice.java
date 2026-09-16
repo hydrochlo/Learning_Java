29dd6cc7e850dd1bc64626ae8121ea39e9bf07cf
Shahadat Hossain <sht.kcn@gmail.com>
2026-09-16 20:35:39 +0600

    removed unnecessary things

--- Projects/SnakeEyeGame/src/com/shahadat/game/Dice.java
+++ Projects/SnakeEyeGame/src/com/shahadat/game/Dice.java
@@ -4,22 +4,10 @@ public class Dice {
     private final int numFaces;
     private int faceValue;
 
-    // Default to a standard 6-sided die
-    public Dice() {
-        this(6);
-    }
-
     public Dice(int numFaces) {
-        if (numFaces < 1) {
-            System.out.println("Enter a valid face value.");
-        }
         this.numFaces = numFaces;
-        roll();
     }
 
-    /**
-     * Rolls the dice and returns the new value.
-     */
     public int roll() {
         faceValue = (int)(Math.random() * this.numFaces + 1);
         return faceValue;
--- Projects/SnakeEyeGame/src/com/shahadat/game/Main.java
+++ Projects/SnakeEyeGame/src/com/shahadat/game/Main.java
@@ -2,12 +2,8 @@ package com.shahadat.game;
 
 public class Main {
     public static void main(String[] args) {
-//        Dice d = new Dice(8);
-//        // d.roll();
-//        System.out.println(d.getFaceValue());
-//        System.out.println(d.getNumFaces());
 
-        int totalSnakeEyes = PlaySnakeEye.numSnakeEyes(6, 13, 100);
+        int totalSnakeEyes = PlaySnakeEye.numSnakeEyes(6, 13, 1000);
         System.out.println("Total snake eyes rolled: " + totalSnakeEyes);
 
     }
